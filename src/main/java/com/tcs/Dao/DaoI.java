000fea5f4dc56ae7e39e84c0e85315ccce36371a
vishalmatale <you@example.com>
2022-08-12 17:28:51 +0530

    SBCHO-7:create Dao Interface

--- src/main/java/com/tcs/Dao/DaoI.java
+++ src/main/java/com/tcs/Dao/DaoI.java
@@ -1,5 +1,27 @@
 package com.tcs.Dao;
 
+import java.util.List;
+
+import com.tcs.model.Employee;
+
 public interface DaoI {
 
+public Object savedata(Employee employee);
+	
+	
+	public Employee getid(int id);
+	
+	public List<Employee>getall(Employee employee);
+	
+	public Employee update(Employee employee);
+	
+   public Employee deleteid(int id); 
+
+    public void deletealldata(Employee employee);
+
+	
 }
+
+	
+	
+
