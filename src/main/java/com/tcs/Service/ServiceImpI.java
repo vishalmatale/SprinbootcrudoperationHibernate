973fcfb112ea928248e1a7aa745aa6de8b88f0c2
vishalmatale <you@example.com>
2022-08-12 17:27:12 +0530

    SBCOH-4:create serviceImpI class

--- src/main/java/com/tcs/Service/ServiceImpI.java
+++ src/main/java/com/tcs/Service/ServiceImpI.java
@@ -1,5 +1,71 @@
 package com.tcs.Service;
 
-public class ServiceImpI {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tcs.Dao.DaoI;
+import com.tcs.model.Employee;
+@Service
+public class ServiceImpI implements ServiceI {
+
+	
+	
+	@Autowired
+	private DaoI daoI;
+
+	@Override
+	public Object savedata(Employee employee) {
+		
+		Object save = daoI.savedata(employee);
+		
+		return save;
+	}
+
+	
+
+	@Override
+	public Employee getid(int id) {
+		
+	    Employee id1 = daoI.getid(id);	
+		return id1;
+	}
+
+	@Override
+	public List<Employee> getall(Employee employee) {
+		
+		List<Employee> list = daoI.getall(employee);
+		return list;
+	}
+
+	@Override
+	public Employee update(Employee employee) {
+		Employee update = daoI.update(employee);
+		return update;
+	}
+
+	@Override
+	public List<Employee> multipleupdate(List<Employee> employee) {
+	
+		
+		return null;
+	}
+
+	
+
+	@Override
+	public Employee deleteid(int id) {
+		Employee id1 = daoI.deleteid(id);
+		
+		return id1;
+	}
+
+	@Override
+	public void deletealldata(Employee employee) {
+		daoI.deletealldata(employee);
+		
+	}
+	
+	
 }
