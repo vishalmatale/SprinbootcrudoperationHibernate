1a668977e7ad09851243b47d0216385456d260ae
vishalmatale <you@example.com>
2022-08-12 17:25:07 +0530

    SBCOH-3:create Service Interface

--- src/main/java/com/tcs/Service/ServiceI.java
+++ src/main/java/com/tcs/Service/ServiceI.java
@@ -1,5 +1,26 @@
 package com.tcs.Service;
 
+import java.util.List;
+
+import com.tcs.model.Employee;
+
 public interface ServiceI {
+public Object savedata(Employee employee);
+
+	
+	public Employee getid(int id);
+	
+	public List<Employee>getall(Employee employee);
+	
+	public Employee update(Employee employee);
+	
+	public List<Employee>multipleupdate(List<Employee>employee);
+	
+	public Employee deleteid(int id); 
+
+    public void deletealldata(Employee employee);
+
 
 }
+
+
